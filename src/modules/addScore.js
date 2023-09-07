61dc09a4437d3a9b264e392adb1cfc1aa5570eeb
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-09-07 14:43:39 +0100

    updated addScore.js file with API

--- src/modules/addScore.js
+++ src/modules/addScore.js
@@ -1,56 +1,79 @@
 class LeaderBoard {
   constructor() {
-    this.scores = 'scores';
-    this.getStorage = JSON.parse(localStorage.getItem(this.scores)) || [];
-    this.name = document.querySelector('.name');
-    this.count = document.querySelector('.score');
-    this.btn = document.querySelector('.input-button');
-    this.form = document.querySelector('.form');
-    this.listContainer = document.querySelector('.scores-container');
-    this.listAdd = document.querySelector('.scores-list');
+    this.scores = "scores";
+    this.name = document.querySelector(".name");
+    this.count = document.querySelector(".score");
+    this.btn = document.querySelector(".input-button");
+    this.form = document.querySelector(".form");
+    this.listContainer = document.querySelector(".scores-container");
+    this.listAdd = document.querySelector(".scores-list");
     this.document = document;
+    this.apiEndPoint =
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/rov8KthJ9rzrWECdKBf4/scores";
+    this.refresh = document.querySelector(".refresh");
   }
 
-  setStorage() {
-    localStorage.setItem(this.scores, JSON.stringify(this.getStorage));
+  clearInput() {
+    this.name.value = "";
+    this.count.value = "";
   }
 
-  displayScores() {
-    this.listAdd.innerHTML = '';
-    if (this.getStorage.length === 0) {
-      const msg = this.document.createElement('h3');
-      msg.textContent = 'No scores yet! Add some!';
-      msg.classList.add('display-msg');
-      this.listContainer.appendChild(msg);
-    } else {
-      this.getStorage.forEach((score) => {
-        this.listAdd.innerHTML += `
-           <li class="scores-item">${score.name}: ${score.count}</li>
-            `;
-      });
-    }
-  }
+  async postScores() {
+    const user = this.name.value;
+    const score = this.count.value;
 
-  addScores() {
-    const scoresObj = {};
-    scoresObj.name = this.name.value;
-    scoresObj.count = this.count.value;
-    scoresObj.id = Math.floor(Math.random() * 10000000);
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        user,
+        score,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
     if (this.name && this.count) {
-      this.getStorage.push(scoresObj);
-      this.setStorage();
-      this.displayScores();
+      await fetch(this.apiEndPoint, options).then((res) => res.json());
     }
 
-    this.name = '';
-    this.count = '';
+    this.clearInput();
+  }
+
+  async getScores() {
+    const res = await fetch(this.apiEndPoint);
+    const data = await res.json();
+    return data;
+  }
+
+  async displayScores() {
+    const list = await this.getScores();
+    this.listAdd.innerHTML = "";
+    if (list.result.length === 0) {
+      const msg = this.document.createElement("h3");
+      msg.textContent = "No scores yet! Add some!";
+      msg.classList.add("display-msg");
+      this.listAdd.appendChild(msg);
+    } else {
+      list.result
+        .sort((a, b) => a.score - b.score)
+        .forEach((score) => {
+          this.listAdd.innerHTML += `
+         <li class="scores-item">${score.user}: ${score.score}</li>
+          `;
+        });
+    }
   }
 
   submitInput() {
-    this.form.addEventListener('submit', () => this.addScores());
-    this.btn.addEventListener('click', () => this.addScores());
-    this.displayScores();
+    this.form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.postScores();
+    });
+  }
+
+  refreshScores() {
+    this.refresh.addEventListener("click", () => this.displayScores());
   }
 }
 export default LeaderBoard;
